28c06bdb9a5921c25a930d9975a691744c4cda05
Sébastien Lavoie <sgdlavoie@gmail.com>
2018-09-02 21:03:01 -0500

    solves Python / Strings / Capitalize

---
+++ hackerrank/python/capitalize.py
@@ -0,0 +1,18 @@
+def solve(s):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    result = ''
+    for index, char in enumerate(s):
+        # if this is the first character in string
+        if index == 0 and char in alphabet:
+            char = char.upper()
+        # if the character is preceded by a space
+        elif char in alphabet and s[index - 1] == ' ':
+            char = char.upper()
+        result += char
+    return result
+
+
+if __name__ == '__main__':
+    print(solve('132 456 Wq  m e'))
+    print(solve('hello   world  lol'))
+    print(solve('hello world'))
